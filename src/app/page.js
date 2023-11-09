25a479bbac76733c6b7484237fb464478aa528e9
rayyanekaputra <bullzhit99@gmail.com>
2023-11-09 15:37:54 +0800

    feat: tambahkan fitur ganti bio dan nama

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,23 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 import './globals.css'
+
 export default function Home() {
+
+  const [bravo, setBravo] = useState(0)
+  const [nama, setNama] = useState('Rayyan Eka Putra')
+
+  function handlerTambahBravo(){
+    setBravo(bravo + 1)
+  }
+
+  function handlerGantiNama(){
+    setNama('Expresidentz')
+  }
+  
+  
   return (
     <div className='body'>
       <div className="banner-container">
@@ -18,22 +35,27 @@ export default function Home() {
           </div>
           <div className="content-header-banner">
             {/* Nama dan Kawan2*/}
-            <h1>Rayyan Eka Putra</h1>
+            <h1>{nama}</h1>
             <div className="bio-nim-header-banner">
             {/* NIM dan BIO*/}
             <p>D121191074</p>
-            <p>Bravo 6, going dark</p>
+            <p>Bravo {bravo}, going dark</p>
             </div>
           </div>
         </div>
         <div className="cta-banner-wrapper">
           {/* Tombol CTA */}
-          <a href=''>
-            <div className='cta-button'>
+            <div className='cta-button' onClick={handlerTambahBravo}>
               <p>Halo!</p>
             </div>
-          </a>
-          
+            <div className='cta-button' 
+            style={{
+              marginTop: '12px'
+            }}
+            
+            onClick={handlerGantiNama}>
+              <p>Ganti Nama</p>
+            </div>
         </div>
       </div>
     </div>
